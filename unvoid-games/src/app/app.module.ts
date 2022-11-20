138bd70be76f8b2bcad6bd2bcc6e1e180fba667e
Rafael Lima <rafaa99@gmail.com>
2022-11-20 18:26:13 -0300

    auth, signup & login components

--- unvoid-games/src/app/app.module.ts
+++ unvoid-games/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './auth/login/login.module';
+import { SignUpModule } from './auth/sign-up/sign-up.module';
 import { HeaderModule } from './header/header.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,11 @@ import { HeaderModule } from './header/header.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    LoginModule,
+    SignUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
